3ffe9f4e006a61ff8cbaeca627184c8148e770df
Ayoub Rekik <ayoubrekik15@gmail.com>
2026-09-04 10:49:39 +0100

    feat: unified single server setup on port 8000

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Serve Angular Single Page Application for all web routes
+Route::get('/{any?}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
